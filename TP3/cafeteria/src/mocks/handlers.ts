cbe12382534bbbabb194e6543fff98bda1f38323
Belen <belenburgos.dev@gmail.com>
2025-11-11 23:13:16 -0300

    entorno tp3

---
+++ TP3/cafeteria/src/mocks/handlers.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.get("/api/menu", () => {
+    return HttpResponse.json([
+      { id: "1", name: "Café", price: 2500 },
+      { id: "2", name: "Tostada", price: 3000 },
+    ]);
+  }),
+
+  http.post("/api/orders", async () => {
+    return HttpResponse.json({ message: "Pedido confirmado" }, { status: 201 });
+  }),
+];
